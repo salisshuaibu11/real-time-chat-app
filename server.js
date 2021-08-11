07dacc6b7be8171adefda1183e285c2f3501502b
salisu <salisshuaibu11@gmail.com>
2021-08-11 11:54:05 +0000

    Used express as our handler for HTTP request

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 let http = require("http"),
-      socketIO = require("socket.io"),
-      fs = require("fs"),
-      server,
-      io;
+  express = require("express"),
+  app = express(),
+  socketIO = require("socket.io"),
+  fs = require("fs"),
+  server,
+  io;
 
-server = http.createServer((req, res) => {
-  fs.readFile(`${__dirname}/index.html`, (err, data) => {
-    res.writeHead(200);
-    res.end(data);
-  });
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
+server = http.Server(app);
 server.listen(5000);
+
 io = socketIO(server);
 
 io.on("connection", (socket) => {
   socket.emit("greeting-from-server", {
-    greeting: "Hello Client"
+    greeting: "Hello Client",
   });
 
-  socket.on("greeting-from-Client", (message) => {
+  socket.on("greeting-from-client", (message) => {
     console.log(message);
   });
 });
